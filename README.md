09adcc6b253b4571b38d093cd3c4c60f344eb001
Emilio Guarino <emilio.guarino@gmail.com>
2022-02-11 11:38:30 -0500

    new app setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tenzies game made with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./style.css"
+
+export default function App() {
+
+    return (
+    <div className="app">
+        <main className="main-el">
+            <h1>Tenzies</h1>
+
+        </main>
+    </div>    
+    )
+}
